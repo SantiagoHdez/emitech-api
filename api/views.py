13229d69a737b8d3a279b9097cf7e8e6626acf84
Santiago Hernandez <santiago.hdezmafara@gmail.com>
2017-04-20 21:20:58 -0500

    Added ProductList view

--- api/views.py
+++ api/views.py
@@ -23,7 +23,6 @@ class AppUserList(APIView):
 
 
 class AppUserDetail(APIView):
-
     def get_object(self, pk):
         try:
             return AppUser.objects.get(pk=pk)
@@ -41,4 +40,19 @@ class AppUserDetail(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
