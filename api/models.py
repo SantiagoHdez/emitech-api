1bd6a74a8525ecbe124c1d128c7d6918948f1c09
Santiago Hernandez <santiago.hdezmafara@gmail.com>
2017-04-20 19:16:07 -0500

    Initial db schema

--- api/models.py
+++ api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class AppUser(models.Model):
+    username = models.CharField(max_length=24)
+    password = models.CharField(max_length=18)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=18)
+    model = models.CharField(max_length=24)
+    code = models.CharField(max_length=18)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    suggested_price = models.DecimalField(max_digits=7, decimal_places=2, null=True)
+
+
+class Stock(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="stocks")
+    unique_idenfitier= models.CharField(max_length=18, null=True)
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+class Cart(models.Model):
+    appuser = models.ForeignKey(AppUser, on_delete=models.PROTECT, related_name="carts")
+    total_cost = models.DecimalField(max_digits=8, decimal_places=2)
+    purchased = models.BooleanField(default=False)
+
+class ProductCart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="products")
+    quantity = models.IntegerField()
