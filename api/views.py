6edcdc335e0593021c4b2400f027ff49a9505069
Santiago Hernandez <santiago.hdezmafara@gmail.com>
2017-04-25 19:58:40 -0500

    Added ProductList view

--- api/views.py
+++ api/views.py
@@ -23,19 +23,19 @@ class AppUserList(APIView):
 
 
 class AppUserDetail(APIView):
-    def get_object(self, pk):
+    def get_appuser(self, pk):
         try:
             return AppUser.objects.get(pk=pk)
         except AppUser.DoesNotExist:
             raise Http404
 
     def get(self, request, pk, format=None):
-        appuser = self.get_object(pk)
+        appuser = self.get_appuser(pk)
         serializer = AppUserSerializer(appuser)
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
-        appuser = self.get_object(pk)
+        appuser = self.get_appuser(pk)
         serializer = AppUserSerializer(appuser, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -49,7 +49,6 @@ class ProductList(APIView):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-
     def post(self, request, format=None):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
@@ -57,9 +56,25 @@ class ProductList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class ProductDetail(APIView):
+    def get_product(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
     def get(self, request, pk, format=None):
         product = Product.objects.get(pk=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        product = self.get_product(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
