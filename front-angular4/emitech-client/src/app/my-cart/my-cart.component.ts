5172e336d4608e64e673a7621f54c12677fb2d8f
Ruben Murga <ruben.murga.d@gmail.com>
2017-05-06 16:22:55 -0500

    adding number of products to eliminate in drop products from cart

--- front-angular4/emitech-client/src/app/my-cart/my-cart.component.ts
+++ front-angular4/emitech-client/src/app/my-cart/my-cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service'
+import { FormGroup, FormControl } from '@angular/forms';
+import {ToastyService, ToastyConfig, ToastOptions, ToastData} from 'ng2-toasty';
 
 @Component({
   selector: 'app-my-cart',
@@ -9,19 +11,34 @@ import { CartService } from '../cart.service'
 export class MyCartComponent implements OnInit {
   dropIdPending;
   cartProducts = []; 
-  constructor(private cartService : CartService) { }
+  dropProductCart : FormGroup;
+  constructor(private cartService : CartService,private toastyService:ToastyService, private toastyConfig: ToastyConfig){
+    this.toastyConfig.theme = 'bootstrap';
+   }
   
   ngOnInit() {
     this.cartProducts =  this.cartService.get_cart_products();
     console.log(this.cartProducts);
+    this.dropProductCart = new FormGroup({
+      numProducts : new FormControl()
+    });
   }
 
   drop_product_pending = function($id){
     this.dropIdPending = $id;
+    this.dropProductCart.reset({
+      numProducts : this.cartProducts[$id].numProductos
+    });
   };
 
   drop_product = function(){
-    this.cartProducts.splice(this.dropIdPending, 1);
+    if(this.dropProductCart.controls.numProducts.value == this.cartProducts[this.dropIdPending].numProductos){
+      this.cartProducts.splice(this.dropIdPending, 1);
+      this.toastyService.success("Se ha eliminado el producto.");
+    }else{
+      this.cartProducts[this.dropIdPending].numProductos = this.cartProducts[this.dropIdPending].numProductos - this.dropProductCart.controls.numProducts.value;
+      this.toastyService.success("Se han eliminado "+this.dropProductCart.controls.numProducts.value+" productos");
+  } 
   };
 
 
