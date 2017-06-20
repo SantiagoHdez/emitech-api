a7b81405e1558f8d44e32510140896139701703f
Santiago Hernandez <santiago.hdezmafara@gmail.com>
2017-06-20 12:08:34 -0500

    Removed Token automatic assigment

--- api/models.py
+++ api/models.py
@@ -4,16 +4,9 @@ import uuid
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
-
 
 # Create your models here.
+
 
 class AppUser(models.Model):
     username = models.CharField(max_length=24)
